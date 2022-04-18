cb78ef8af802cd354bc3a4b7c7b173aee131db78
Zach Zolton <zach.zolton@sproutsocial.com>
2022-04-18 15:28:24 -0400

    Make diagnostic hints readable for color scheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,13 +1,18 @@
 vim.cmd [[
-set termguicolors
-try
-  colorscheme solarized8_high
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-endtry
+    set termguicolors
+    try
+      colorscheme solarized8_high
+    catch /^Vim\%((\a\+)\)\=:E185/
+      colorscheme default
+    endtry
 
-" If you need to adjust the modal popup background color
-"highlight Pmenu ctermbg=blue guibg=blue
-  
-set background=light
+    set background=light
+]]
+
+vim.cmd [[
+    " If you need to adjust the modal popup background color
+    "highlight Pmenu ctermbg=blue guibg=blue
+
+    " Make floating diagnostic hints readable with my light solarized theme...
+    highlight DiagnosticHint ctermfg=7 guifg=grey
 ]]

ff9f1ed5c4ba601b489151ee51498409d070b668
Zach Zolton <zach.zolton@sproutsocial.com>
2022-05-03 14:05:37 -0400

    Hack nvim-tree options that moved to globals
    Not sure why they wanted to take some stuff out of the setup method. Oh
    well, read the Github issue in the diff for more info.

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -21,6 +21,75 @@ vim.g.nvim_tree_icons = {
   },
 }
 
+-- from https://github.com/LunarVim/Neovim-from-scratch/issues/124
+-- TEMPORARY HACK
+vim.g["nvim_tree_git_hl"] = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
+vim.g["nvim_tree_highlight_opened_files"] = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
+vim.g["nvim_tree_root_folder_modifier"] = ':~' --This is the default. See :help filename-modifiers for more options
+vim.g["nvim_tree_add_trailing"] = 1 --0 by default, append a trailing slash to folder names
+vim.g["nvim_tree_group_empty"] = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
+vim.g["nvim_tree_icon_padding"] = ' ' --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
+vim.g["nvim_tree_symlink_arrow"] = ' >> ' -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
+vim.g["nvim_tree_respect_buf_cwd"] = 1 --0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
+vim.g["nvim_tree_create_in_closed_folder"] = 1 --0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
+vim.g["nvim_tree_special_files"] = { ["README.md"] = 1, Makefile = 1, MAKEFILE = 1 } -- List of filenames that gets highlighted with NvimTreeSpecialFile
+vim.g["nvim_tree_show_icons"] = {
+      git = 1,
+      folders = 0,
+      files = 0,
+      folder_arrows = 0,
+    }
+--If 0, do not show the icons for one of 'git' 'folder' and 'files'
+--1 by default, notice that if 'files' is 1, it will only display
+--if nvim-web-devicons is installed and on your runtimepath.
+--if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
+--but this will not work when you set renderer.indent_markers.enable (because of UI conflict)
+
+-- default will show icon by default if no icon is provided
+-- default shows no icon by default
+vim.g["nvim_tree_icons"] = {
+          default = "",
+          symlink = "",
+          git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌"
+        },
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        }
+      }
+
+vim.cmd "nnoremap <C-n> :NvimTreeToggle<CR>"
+vim.cmd "nnoremap <leader>r :NvimTreeRefresh<CR>"
+vim.cmd "nnoremap <leader>n :NvimTreeFindFile<CR>"
+-- More available functions:
+-- NvimTreeOpen
+-- NvimTreeClose
+-- NvimTreeFocus
+-- NvimTreeFindFileToggle
+-- NvimTreeResize
+-- NvimTreeCollapse
+-- NvimTreeCollapseKeepBuffers
+
+vim.cmd [[set termguicolors]] -- this variable must be enabled for colors to be applied properly
+
+-- a list of groups can be found at `:help nvim_tree_highlight`
+vim.cmd [[highlight NvimTreeFolderIcon guibg=blue]]
+
+-- END TEMPORARY HACK
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -45,10 +114,6 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -81,7 +146,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -97,16 +161,5 @@ nvim_tree.setup {
   trash = {
     cmd = "trash",
     require_confirm = true,
-  },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
   },
 }

f7f394184931ea3fe940fbce41d2ae590260fdd1
Zach Zolton <zach.zolton@sproutsocial.com>
2022-05-03 13:49:54 -0400

    Use new method to configure lsp-installer plugin
    Incorporating ideas from: https://github.com/williamboman/nvim-lsp-installer/discussions/636
    
    I still kept the language-specific settings in their own file.

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,8 +1,35 @@
-local status_ok, _ = pcall(require, "lspconfig")
+local installer_status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not installer_status_ok then
+	return
+end
+lsp_installer.setup {}
+
+local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
   return
 end
 
-require "user.lsp.lsp-installer"
+local lsps = {
+    "jsonls", -- JSON
+    "sumneko_lua", -- Lua
+    "jedi_language_server", -- Python
+    "intelephense", -- PHP
+}
+
+-- Using this list of desired language servers, set them up with capabilites
+-- and, if we have a LSP-specific settings file then include that too
+for _, name in ipairs(lsps)
+do
+    local lsp_settings_found, lsp_settings = pcall(require, "user.lsp.settings." .. name)
+    local opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+    }
+    if lsp_settings_found then
+	 	opts = vim.tbl_deep_extend("force", lsp_settings, opts)
+    end
+    lspconfig[name].setup(opts)
+end
+
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"

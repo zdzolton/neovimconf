6078257c175aae43e10a5e37226c6e03aab0640f
Zach Zolton <zach.zolton@sproutsocial.com>
2022-05-04 10:18:52 -0400

    Ensure we install desired language servers

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,24 +1,35 @@
+local desired_language_servers = {
+    "intelephense", -- PHP
+    "jedi_language_server", -- Python
+    "jsonls", -- JSON
+    "sumneko_lua", -- Lua
+}
+
 local installer_status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not installer_status_ok then
 	return
 end
-lsp_installer.setup {}
+
+lsp_installer.setup {
+    ensure_installed = desired_language_servers, -- ensure these servers are always installed
+    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+}
 
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
   return
 end
 
-local lsps = {
-    "jsonls", -- JSON
-    "sumneko_lua", -- Lua
-    "jedi_language_server", -- Python
-    "intelephense", -- PHP
-}
-
 -- Using this list of desired language servers, set them up with capabilites
 -- and, if we have a LSP-specific settings file then include that too
-for _, name in ipairs(lsps)
+for _, name in ipairs(desired_language_servers)
 do
     local lsp_settings_found, lsp_settings = pcall(require, "user.lsp.settings." .. name)
     local opts = {

afc5c1ff687ca13c6e4deb526f1d2e702f6451d4
Zach Zolton <zach.zolton@sproutsocial.com>
2022-09-29 10:09:40 -0400

    Add keymap to quit window without saving
    I aligns better with my other keymaps for window navigation

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,6 +22,7 @@ keymap('n', '<C-h>', '<C-w>h', opts)
 keymap('n', '<C-j>', '<C-w>j', opts)
 keymap('n', '<C-k>', '<C-w>k', opts)
 keymap('n', '<C-l>', '<C-w>l', opts)
+keymap('n', '<C-q>', '<cmd>q!<cr>', opts)
 
 -- Nvimtree
 keymap('n', '<leader>e', ':NvimTreeToggle<cr>', opts)

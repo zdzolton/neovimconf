819ab939e3eb3d0fe362a01e1657de23486895a0
Zach Zolton <zach.zolton@sproutsocial.com>
2023-03-16 13:45:49 -0400

    Use textwidth for macchiato if set

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,5 @@
-vim.opt_local.formatprg = 'black-macchiato'
+local macchiato_line_length = 80
+if (vim.o.textwidth or 0) > 0 then
+   macchiato_line_length = vim.o.textwidth
+end
+vim.opt_local.formatprg = 'black-macchiato -l ' .. macchiato_line_length
